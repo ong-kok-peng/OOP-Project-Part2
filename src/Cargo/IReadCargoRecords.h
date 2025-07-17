8cdfbd0255b1afd35e35b7838793535f17986808
bayuetong <166102808+bayuetong@users.noreply.github.com>
2025-07-17 16:57:02 +0800

    Update IReadCargoRecords.h

--- src/Cargo/IReadCargoRecords.h
+++ src/Cargo/IReadCargoRecords.h
@@ -4,6 +4,7 @@
 #define I_READ_CARGO_RECORDS_H
 
 #include "Cargo.h"
+#include <vector>
 
 class IReadCargoRecords{
 public:
@@ -11,7 +12,7 @@ public:
 	virtual Cargo getCargo(int index) = 0;
 	virtual void sortCargoByTime() = 0;
 	virtual void sortCargoByQuantity() = 0;
-	virtual std::vector<Cargo> getAllRecords() const = 0;
+	virtual std::vector <Cargo> getAllRecords() const = 0;
     virtual ~IReadCargoRecords() = default;
 };
 
