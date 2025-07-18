ff41efb830e9758d077a7e3e0bf6d5d29bd656ce
bayuetong <166102808+bayuetong@users.noreply.github.com>
2025-07-18 22:32:27 +0800

    Update CargoExportData.cpp

--- src/Exporters/CargoExportData.cpp
+++ src/Exporters/CargoExportData.cpp
@@ -3,11 +3,11 @@ Created by: Yeo Zi Xuan Augustine (2403343)
 Date: 18/7/2025
 */
 
+#include "CargoExportData.h"
+#include "../FileManager/FileWriter.h"
 #include <iostream>
 #include <string>
 using namespace std;
-
-#include "CargoExportData.h"
 
 CargoExportData::CargoExportData(IReadCargoRecords& readCR) : readCR{ readCR } {}
 
@@ -18,5 +18,23 @@ void CargoExportData::exportRecords() {
         cout << "There are no records. Add a new record or import from a file.\n";
         return;
     }
-    
+
+    cout << "Enter filename to export to: ";
+    string filename;
+    cin >> filename;
+
+    FileWriter writer;
+    string header = "ID,Destination,Time,Quantity";
+    if (!writer.writeToFile(filename, header, false)) {
+        cout << "Failed to open file: " << filename << "\n";
+        return;
+    }
+
+    for (int i = 0; i < readCR.getRecordsSize(); ++i) {
+        Cargo c = readCR.getCargo(i);
+        string line = c.getId() + "," + c.getDestination() + "," + c.getTime() + "," + to_string(c.getQuantity());
+        writer.writeToFile(filename, line, true);
+    }
+
+    cout << "Export successful to: " << filename << "\n";
 }
